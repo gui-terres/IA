49818abd70890e6a49866014eba4f5aa660ef910
Guilherme Terres <guirterres@gmail.com>
2024-10-30 22:48:56 -0300

    Adjust linear regression functions and parameters

--- t1-aprendizado-supervisionado/alegrete.py
+++ t1-aprendizado-supervisionado/alegrete.py
@@ -3,7 +3,6 @@ import numpy as np
 def compute_mse(b, w, data):
     land_area = data.iloc[:, 0]
     real_land_price = data.iloc[:, 1]
-
     predicted_land_price = b + w*land_area
 
     return np.mean((predicted_land_price - real_land_price) ** 2)
@@ -12,15 +11,16 @@ def compute_mse(b, w, data):
 def step_gradient(b, w, data, alpha):
     land_area = data.iloc[:, 0]
     real_land_price = data.iloc[:, 1]
-
     predicted_land_price = b + w*land_area
-
     error = predicted_land_price - real_land_price
 
     gradient_w = (1 / len(real_land_price)) * np.sum(error * land_area)
     gradient_b = (1 / len(real_land_price)) * np.sum(error)
 
-    return w - alpha * gradient_w, b - alpha * gradient_b
+    w -= alpha * gradient_w
+    b -= alpha * gradient_b
+
+    return w, b
 
 
 def fit(data, b, w, alpha, num_iterations):
@@ -28,8 +28,8 @@ def fit(data, b, w, alpha, num_iterations):
     b_list = []
 
     for i in range(num_iterations):
-        buffer_w, buffer_b = step_gradient(b, w, data, alpha)
-        w_list.append(buffer_w)
-        b_list.append(buffer_b)
+        w, b = step_gradient(b, w, data, alpha)
+        w_list.append(w)
+        b_list.append(b)
         
-    return w_list, b_list
+    return b_list, w_list
