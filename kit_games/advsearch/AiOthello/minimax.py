758cc376b7a8bd0c2a3b530776b4b915d76ab28b
Guilherme Terres <guirterres@gmail.com>
2025-01-06 19:16:17 -0300

    Rename agent

--- kit_games/advsearch/AiOthello/minimax.py
+++ kit_games/advsearch/AiOthello/minimax.py
@@ -13,10 +13,10 @@ def minimax_move(state, max_depth: int, eval_func: Callable) -> Tuple[int, int]:
     :return: (int, int) tuple representing the x, y coordinates of the chosen move.
     """
     current_player = state.player
-    _, best_move = max(state, float('-inf'), float('inf'), eval_func, max_depth, 0, current_player)
+    _, best_move = max_value_1(state, float('-inf'), float('inf'), eval_func, max_depth, 0, current_player)
     return best_move
 
-def max(state, alpha, beta, eval_func, max_depth, depth, player) -> Tuple[float, Tuple[int, int]]:
+def max_value_1(state, alpha, beta, eval_func, max_depth, depth, player) -> Tuple[float, Tuple[int, int]]:
     if state.is_terminal() or (max_depth != -1 and depth >= max_depth):
         return eval_func(state, player), None
 
@@ -25,7 +25,7 @@ def max(state, alpha, beta, eval_func, max_depth, depth, player) -> Tuple[float,
 
     for action in state.legal_moves():
         successor = state.next_state(action)
-        value, _ = min(successor, alpha, beta, eval_func, max_depth, depth + 1, player)
+        value, _ = min_value_1(successor, alpha, beta, eval_func, max_depth, depth + 1, player)
         if value > max_value:
             max_value = value
             best_move = action
@@ -35,7 +35,7 @@ def max(state, alpha, beta, eval_func, max_depth, depth, player) -> Tuple[float,
 
     return value, best_move
 
-def min(state, alpha, beta, eval_func, max_depth, depth, player) -> Tuple[float, Tuple[int, int]]:
+def min_value_1(state, alpha, beta, eval_func, max_depth, depth, player) -> Tuple[float, Tuple[int, int]]:
     if state.is_terminal() or (max_depth != -1 and depth >= max_depth):
         return eval_func(state, player), None
 
@@ -44,7 +44,7 @@ def min(state, alpha, beta, eval_func, max_depth, depth, player) -> Tuple[float,
 
     for action in state.legal_moves():
         successor = state.next_state(action)
-        value, _ = max(successor, alpha, beta, eval_func, max_depth, depth + 1, player)
+        value, _ = max_value_1(successor, alpha, beta, eval_func, max_depth, depth + 1, player)
         if value < min_value:
             min_value = value
             best_move = action
